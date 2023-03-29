d73a0e300f5f61b1255437acd109a5499e236acf
jin4333 <vhfhflsksk@gmail.com>
2023-03-29 15:53:07 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "productgion" ? "/vue-corona" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-corona" : "/",
   outputDir: "docs",
 };
